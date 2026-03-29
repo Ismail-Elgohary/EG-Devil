8c39218037facaa784cc3a74f9f7a2b9ecc2ab31
Ismail-Elgohary <ismailelgohary173@gmail.com>
2026-03-29 21:03:43 +0200

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-//allowedDevOrigins: ["127.0.0.1", "localhost"],
+const nextConfig = {
   images: {
-    domains: ["images.pexels.com"],
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "fakestoreapi.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "images.asos-media.com" },
+      { protocol: "https", hostname: "ui-avatars.com" },
+      { protocol: "https", hostname: "m.media-amazon.com" },
+    ],
   },
+  allowedDevOrigins: ["127.0.0.1", "localhost"],
 };
 
 export default nextConfig;

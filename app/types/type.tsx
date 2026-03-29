ca8adb84439d36d9d54ea651878ad78cb0b88094
Ismail-Elgohary <ismailelgohary173@gmail.com>
2026-03-29 20:42:33 +0200

    updates

--- app/types/type.tsx
+++ app/types/type.tsx
@@ -1,18 +1,11 @@
 type Tpost = {
   id: number;
   name: string;
-  description: string;
+  description?: string;
   image: string;
   price: number;
 };
 
 
-type Tslider = {
-  id: number;
-  brandIcon: IconDefinition;
-  subtitle: string;
-  title: string;
-  image: string;
-  bgColor: string;
-};
-export type { Tpost, Tslider};
+
+export type { Tpost};

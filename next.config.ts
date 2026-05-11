e1a6d750c979cba6a105d31a70c7617e002e5662
Ismail-Elgohary <ismailelgohary173@gmail.com>
2026-05-11 06:55:04 +0300

    finished

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,21 @@
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{ protocol: "https", hostname: "images.unsplash.com" },
-			{ protocol: "https", hostname: "fakestoreapi.com" },
-			{ protocol: "https", hostname: "images.pexels.com" },
-			{ protocol: "https", hostname: "images.asos-media.com" },
-			{ protocol: "https", hostname: "ui-avatars.com" },
-			{ protocol: "https", hostname: "m.media-amazon.com" },
-			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
-			{ protocol: "https", hostname: "external-content.duckduckgo.com" },
-		],
-	},
-	allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000"],
+ images: {
+  remotePatterns: [
+   { protocol: "https", hostname: "images.unsplash.com" },
+   { protocol: "https", hostname: "fakestoreapi.com" },
+   { protocol: "https", hostname: "images.pexels.com" },
+   { protocol: "https", hostname: "images.asos-media.com" },
+   { protocol: "https", hostname: "ui-avatars.com" },
+   { protocol: "https", hostname: "m.media-amazon.com" },
+   { protocol: "https", hostname: "avatars.githubusercontent.com" },
+   { protocol: "https", hostname: "external-content.duckduckgo.com" },
+   {
+    protocol: "https",
+    hostname: "lh3.googleusercontent.com",
+   },
+  ],
+ },
+ allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000"],
 };
 
 export default nextConfig;
--- proxy.ts
+++ proxy.ts
@@ -2,29 +2,43 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(req: NextRequest) {
-	const { pathname } = req.nextUrl;
+ const { pathname } = req.nextUrl;
 
-	const publicPages = ["/Login", "/Register", "/ForgotPassword"];
-	const protectedPages = ["/Profile", "/addProduct"];
+ const publicPages = ["/Login", "/Register", "/ForgotPassword"];
+ const userPages = ["/Profile", "/addProduct"];
+ const adminPages = ["/dashboard"];
 
-	if (publicPages.some(page => pathname.startsWith(page))) {
-		return NextResponse.next();
-	}
+ if (publicPages.some(page => pathname.startsWith(page))) {
+  return NextResponse.next();
+ }
 
-	const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+ const token = await getToken({
+  req,
+  secret: process.env.NEXTAUTH_SECRET,
+ });
 
-	if (protectedPages.some(page => pathname.startsWith(page)) && !token) {
-		return NextResponse.redirect(new URL("/Login", req.url));
-	}
+ if (
+  (userPages.some(p => pathname.startsWith(p)) ||
+   adminPages.some(p => pathname.startsWith(p))) &&
+  !token
+ ) {
+  return NextResponse.redirect(new URL("/Login", req.url));
+ }
 
-	return NextResponse.next();
+ if (
+  adminPages.some(p => pathname.startsWith(p)) &&
+  token?.role !== "admin"
+ ) {
+  return NextResponse.redirect(new URL("/", req.url));
+ }
+
+ return NextResponse.next();
 }
+
 export const config = {
-	matcher: [
-		"/Profile/:path*",
-		"/Login/:path*",
-		"/Register/:path*",
-		"/ForgotPassword/:path*",
-		"/addProduct/:path*"
-	],
+ matcher: [
+  "/Profile/:path*",
+  "/addProduct/:path*",
+  "/dashboard/:path*",
+ ],
 };
